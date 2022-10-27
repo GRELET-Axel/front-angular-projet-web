237dbf221388386423a6eaacc25e017f4f2a8a44
clemdm44 <55748026+clemdm44@users.noreply.github.com>
2022-10-27 17:03:06 +0200

    getCampus

--- src/app/campus/campus.component.ts
+++ src/app/campus/campus.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { first } from 'rxjs';
+import { campus } from '../models/campus';
+import { CampusService } from '../_services/campus.service';
 
 @Component({
   selector: 'app-campus',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CampusComponent implements OnInit {
 
-  constructor() { }
+  public displayedColumns = ['nomCampus'];
+  campuss: campus[] = [];
+  public dataSource = new MatTableDataSource<campus>();
+
+  sortFunction: ((data: campus[], sort: MatSort) => campus[]) | undefined;
+  titre = '';
+
+  @ViewChild(MatSort) sort: MatSort | undefined;
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  constructor(
+    private campusService: CampusService,
+  ) { }
 
   ngOnInit(): void {
+    console.log(this.campuss)
+    this.campusService.getCampuss()
+      .pipe(first())
+      .subscribe(
+        value => {
+          this.campuss = value
+          this.dataSource.data = this.campuss;
+          console.log(this.campuss)
+        });    
+        this.titre = 'Gérer les Campus';
   }
-
 }
