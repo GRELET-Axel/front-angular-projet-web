60cb8255d18729450811e810774e3db826482c6f
FERRE24Mo <morgan.ferre85@gmail.com>
2022-10-28 11:14:21 +0200

    ajout class

--- src/app/models/Participant.ts
+++ src/app/models/Participant.ts
@@ -1,13 +1,16 @@
 
-export class Participant {
-    public pseudo: string ,
-    public lastName: string ,
-    public firstName: string ,
-    public phone: string,
-    public mail: string,
-    public admin: boolean,
-    public active: boolean ,
-    //public registrations: Registration[];
-    public password: string,
-   // public urlPicture: string;
-  } 
+  export class Participant {
+    constructor(
+        public prenom: string ,
+        public nom: string ,
+        public tel: string,
+        public mail: string,
+        public admin: boolean,
+        public actif: boolean ,
+        //public registrations: Registration[];
+        public password: string
+       // public urlPicture: string;
+    ) {}
+    
+  }
+  
